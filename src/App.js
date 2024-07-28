c14a69cee8c631dc46621c4bb759fb1ccdb4deba
Nikhil Rawal <rawal.nikhil29@gmail.com>
2024-07-28 15:33:55 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome from Macbook</h1>
+      <h1 className="bg-slate-800">Regards, Nikhil Rawal</h1>
+    </div>
+  );
+}
+
+export default App;
