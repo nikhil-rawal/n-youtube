b33b543a97ad0b07aef54dd8066de7932bdf4ab5
Nikhil Rawal <rawal.nikhil29@gmail.com>
2024-07-31 07:25:05 -0400

    sidebar-center

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Head = () => {
+  const hoverCSS = "hover:bg-gray-200 hover:rounded-full";
+
   return (
     <div className="grid grid-flow-col p-2 m-2">
       <div className="flex col-span-2 justify-start">
         <img
-          className="h-8 mx-3 cursor-pointer hover:bg-gray-100 hover:rounded-full"
+          className={`h-9 mx-3 cursor-pointer ${hoverCSS}`}
           alt="menu"
           src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp&w=256"
         />
@@ -15,10 +17,10 @@ const Head = () => {
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGJrpSqrv7Va8wkAJCoRTsHWDJyXJEe_ypDw&s"
         />
       </div>
-      <div className="flex col-span-8 justify-center">
+      <div className="flex col-span-8 justify-center ">
         <div className="flex">
           <input
-            className="w-96 h-9 p-4 rounded-l-full border border-solid outline-none"
+            className="w-96 h-9 p-4 rounded-l-full border border-solid outline-none "
             type="text"
             placeholder="Search"
           />
@@ -31,34 +33,34 @@ const Head = () => {
           </button>
         </div>
         <img
-          className="h-7 mx-5 cursor-pointer"
+          className={`h-7 mx-5 cursor-pointer ${hoverCSS}`}
           alt="search-using-voice"
           src="https://www.svgrepo.com/show/44666/voice-recorder.svg"
         />
       </div>
-      <div className="flex col-span-2 justify-end">
+      <div className="flex col-span-2 justify-end ">
         <img
-          className="h-8 mx-4 cursor-pointer"
+          className={`h-8 mx-4 cursor-pointer ${hoverCSS}`}
           alt="create-video"
           src="https://static.thenounproject.com/png/3750242-200.png"
         />
         <img
-          className="h-8 mx-4 cursor-pointer"
+          className={`h-8 mx-4 cursor-pointer ${hoverCSS}`}
           alt="notifications-icon"
           src="https://static.vecteezy.com/system/resources/previews/015/934/666/original/bell-icon-simple-element-symbol-for-template-design-can-be-used-for-website-and-mobile-application-vector.jpg"
         />
         <img
-          className="h-8 mx-4 cursor-pointer"
+          className={`h-8 mx-4 cursor-pointer ${hoverCSS}`}
           alt="user-icon"
           src="https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"
         />
         <img
-          className="h-8 mx-4 cursor-pointer"
+          className={`h-8 mx-4 cursor-pointer ${hoverCSS}`}
           alt="night-mode"
           src="https://cdn4.iconfinder.com/data/icons/remixicon-weather/24/moon-clear-line-1024.png"
         />
         <img
-          className="h-8 mx-4 cursor-pointer"
+          className={`h-8 mx-4 cursor-pointer ${hoverCSS}`}
           alt="day-mode"
           src="https://cdn3.iconfinder.com/data/icons/weather-free-2/32/Weather_Free_Outline_day-weather-sun-1024.png"
         />
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="flex flex-col text-center">
+      <div className="my-2 py-2 hover:bg-gray-200 hover:rounded-2xl">
+        <img
+          className="h-10 m-auto"
+          alt="home-icon"
+          src="https://cdn0.iconfinder.com/data/icons/typicons-2/24/home-512.png"
+        />
+        <h1>Home</h1>
+      </div>
+      <div className="my-2 py-2 hover:bg-gray-200 hover:rounded-2xl">
+        <img
+          className="h-10 m-auto"
+          alt="shorts-icon"
+          src="https://freelogopng.com/images/all_img/1685029929youtube-shorts-logo-black.png"
+        />
+        <h1>Shorts</h1>
+      </div>
+      <div className="my-2 py-2 hover:bg-gray-200 hover:rounded-2xl">
+        <img
+          className="h-10 m-auto"
+          alt="subscriptions-icon"
+          src="https://cdn3.iconfinder.com/data/icons/social-media-2487/24/subscription-512.png"
+        />
+        <h1>Subscriptions</h1>
+      </div>
+      <div className="my-2 py-2 hover:bg-gray-200 hover:rounded-2xl">
+        <img
+          className="h-10 m-auto"
+          alt="home-icon"
+          src="https://static-00.iconduck.com/assets.00/youtube-music-icon-512x512-tzy5jsl3.png"
+        />
+        <h1>YouTube Music</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
