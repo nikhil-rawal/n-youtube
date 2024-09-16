9bd24ff48bc2736aef2071083596d853afb43e13
Nikhil Rawal <59576734+nikhil-rawal@users.noreply.github.com>
2024-09-16 18:49:46 -0400

    Just a thought for searchResults

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -8,7 +8,12 @@ const SearchResults = () => {
   console.log("results", results);
   console.log("parsed", parsedResults);
 
-  return (
+// redux to send data, then fetch data in commentResults??? (haven't tried) (better approach?)
+// useLocation RRD to send data and use here as soon as data is loaded. (till now unsuccessful) (will read documentation!!)
+// want to make function?? (export const _____) (better than redux??)
+
+
+return (
     <div className="ml-20">
       <h1>Search Results for: {query}</h1>
       <pre>{JSON.stringify(parsedResults, null, 2)}</pre>
